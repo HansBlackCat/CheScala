4dd7f9da383a81949d62e142ee4064f72f6610d4
HansBlackCat <jeremy3599@snu.ac.kr>
2020-05-24 16:26:23 +0900

    2020-05-24 16:26:21

--- src/main/scala/com/hansblackcat/Chess/Root.scala
+++ src/main/scala/com/hansblackcat/Chess/Root.scala
@@ -1,5 +1,12 @@
 package com.hansblackcat.Chess
 
 class Root {
-    
+    implicit class ExLocationMaker (row: Char) {
+        def #> (colum: Int) = {
+            ExLocation(row +: colum.toString)
+        }
+    }
+
+    val gridKeys = for (i <- 'a' to 'h'; j <- 1 to 8) yield i +: j.toString()
+    val gridValue = 4
 }
--- src/main/scala/com/hansblackcat/Chess/TypeDef.scala
+++ src/main/scala/com/hansblackcat/Chess/TypeDef.scala
@@ -1,16 +1,47 @@
 package com.hansblackcat.Chess
 
 trait Location
-case class ExLocation(location: String) extends Location
+case class ExLocation(location: String) extends Location {
+    val a = location.toCharArray().map(_.toInt)
+    val toArrLoc = Array(a(0) - 97, a(1) - 49)
+    def toStrLoc(toArrLoc: Array[Int]): String = (toArrLoc(0)+97).toChar.toString ++ (toArrLoc(1)+1).toString
+    require(a.length < 3 && 0 <= toArrLoc(0) && toArrLoc(0) < 8 && 0 <= toArrLoc(1) && toArrLoc(1) < 8)
+
+    def + (ipt: Array[Int]): ExLocation = {
+        require(ipt.length < 3)
+        val thisArr = this.toArrLoc
+        ExLocation(toStrLoc(Array(thisArr(0)+ipt(0), thisArr(1)+ipt(1))))
+    }
+    def + (ipt: (Int, Int)): ExLocation = {
+        val tmp = Array(ipt._1, ipt._2)
+        this + tmp
+    } 
+    def - (ipt: Array[Int]): ExLocation = {
+        this.+(ipt.map(_*(-1)))
+    }
+    def - (ipt: (Int, Int)): ExLocation = {
+        val tmp = Array(ipt._1, ipt._2)
+        this - tmp
+    } 
+    def R (row: Int): ExLocation = {
+        this + Array(row, 0)
+    }
+    def C (colum: Int): ExLocation = {
+        this + Array(0, colum)
+    }
+}
+
+trait PGNComment
+case class Comment(contents: String) extends PGNComment
 
 trait PGNTagPairs
-case class Event(contents: String) extends PGNTagPairs
-case class Site(contents: String) extends PGNTagPairs
-case class Date(contents: String) extends PGNTagPairs
-case class Round(contents: String) extends PGNTagPairs
-case class White(contents: String) extends PGNTagPairs
-case class Black(contents: String) extends PGNTagPairs
-case class Result(contents: String) extends PGNTagPairs
+case class TPEvent(contents: String) extends PGNTagPairs
+case class TPSite(contents: String) extends PGNTagPairs
+case class TPDate(contents: String) extends PGNTagPairs
+case class TPRound(contents: String) extends PGNTagPairs
+case class TPWhite(contents: String) extends PGNTagPairs
+case class TPNBlack(contents: String) extends PGNTagPairs
+case class TPResult(contents: String) extends PGNTagPairs
 
 trait PGNPieceKind
 case object Phone extends PGNPieceKind
@@ -22,13 +53,17 @@ case object Rook extends PGNPieceKind
 
 trait PGNSpecial
 case object PGNNone extends PGNSpecial
+case object Capture extends PGNSpecial
+case object QSCastling extends PGNSpecial
+case object KSCastling extends PGNSpecial
+case object Promotion extends PGNSpecial
+case object CheckingMove extends PGNSpecial
+case object CheckMateingMove extends PGNSpecial
 
-
-trait PGNMoveText extends PGNPieceKind
+trait PGNMoveText extends PGNPieceKind with PGNSpecial with Location
 case class MoveRound(round: Int) extends PGNMoveText
-case class WhiteLoc(who: PGNPieceKind, where: Location)
-case class BlackLoc(who: PGNPieceKind, where: Location)
+case class White(who: PGNPieceKind, where: Location, what: PGNSpecial*) extends PGNMoveText
+case class Black(who: PGNPieceKind, where: Location, what: PGNSpecial*) extends PGNMoveText
 
-
-trait PGN extends Location
+trait PGN extends PGNTagPairs with PGNMoveText with PGNComment
 
